161ea6090b324b29c36c34a33b1564b0f32e9394
Serroda <jesuscarclem30@gmail.com>
2023-11-27 15:43:09 +0100

    Fix webmanifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,26 +6,27 @@ export default defineNuxtConfig({
   css: [
     "assets/css/main.css"
   ],
-  pwa:{
-   registerType:'autoUpdate',
-   manifest:{
-    name: 'Dots by Jesús',
-    short_name: 'Dots',
-    description: 'Dots for play',
-    theme_color: '#0D0D0D',
-    icons: [
-      {
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
-   }
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      lang: "es",
+      name: 'Dots by Jesús',
+      short_name: 'Dots',
+      description: 'Dots for play',
+      theme_color: '#0D0D0D',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
   },
   app: {
     baseURL: "/dots/",
@@ -36,6 +37,10 @@ export default defineNuxtConfig({
       meta: [{
         "name": "theme-color",
         "content": "#0D0D0D",
+      }],
+      link: [{
+        rel: "manifest",
+        href: "/dots/manifest.webmanifest"
       }],
       viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
     }
